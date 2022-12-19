ae036244401f45d4302868bdabe8bc822496a5f7
Alexandre <AlexandreOSouza>
2022-12-19 17:51:09 -0300

    create notification test

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from './content';
 
-test('it should be able to create a notification content', () => {
-  const content = new Content('You recive a new notification');
+describe('Notification content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('You recive a new notification');
 
-  expect(content).toBeTruthy();
-});
+    expect(content).toBeTruthy();
+  });
 
-test('it should not be able to create a new notification content with less then 5 characters', () => {
-  expect(() => new Content('aaa')).toThrow();
-});
+  it('should not be able to create a new notification content with less then 5 characters', () => {
+    expect(() => new Content('aaa')).toThrow();
+  });
 
-test('it should not be able to create a new notification content with more then 240 characters', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow();
+  it('should not be able to create a new notification content with more then 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
 });
